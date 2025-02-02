8a1fc8b97abb116abe16a2575706872565da4a76
stepnkas <001gradus100@mail.ru>
2025-02-03 01:11:25 +0500

    portfoliodemo

---
+++ app/mycard.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Card, Divider } from "antd";
+import { Typography } from "antd";
+import styles from "./page.module.css";
+
+const { Title } = Typography;
+
+interface ProjectCard {
+  glav: string;
+  description: string;
+}
+
+const MyCard: React.FC<ProjectCard> = ({ glav, description }) => {
+  return (
+      <Card className={styles.card_content}>
+        <h3>{glav}</h3>
+        <p>{description}</p>
+      </Card>
+  );
+};
+
+export default MyCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,82 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import { Card, Layout, List } from "antd";
+import { Header } from "antd/es/layout/layout";
+import Link from "next/link";
+import MyCard from "./mycard";
 
-export default function Home() {
+const data = [
+  '001gradus100@mail.com',
+  '@ztepwrer (telegram)',
+  'sungradus1@gmail.com',
+  '+79999999999',
+];
+
+const Home: React.FC = () => {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <>
+      <Layout>
+        <Header className={styles.headerst}>
+          <div className={styles.header_conteiner}>
+            <Link href="/" className={styles.header_content}>
+              <div>Главная</div>
+            </Link>
+            <Link href="#about" className={styles.header_content}>
+              <div>Обо мне</div>
+            </Link>
+            <Link href="#projects" className={styles.header_content}>
+              <div>Проекты</div>
+            </Link>
+            <Link href="#contact" className={styles.header_content}>
+              <div>Контакты</div>
+            </Link>
+          </div>
+        </Header>
+        <div className={styles.introduction}>
+          <h1 id="about">Сайт обо мне</h1>
+          <p className={styles.gg}>
+            Привет, я Каськов Степан, набирающийся опыта программист, который создаёт
+            пластичные и полезные приложения и сайты. Сдесь вы можете увидеть проекты, демострирующие мои навыки в различных технологиях,
+            над которыми я работал (или работаю).
+            А также мои контакты для работы с вами. И спасибо за внимание.
+          </p>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        <h2 id="projects" className={styles.gggc}>Карточки моих проектов</h2>
+        <div className={styles.card_conteiner}>
+          <MyCard
+            glav="Bookcrossing в VK-mini-app на React"
+            description="Приложение для получения книг от других людей, с базой данных, где хранятся книги и пользователи."
           />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+          <MyCard
+            glav="Сайт о мероприятии (JavaScript, HTML, CSS, SCSS)"
+            description="Сайт для отображения информации о мероприятии, с возможностью регистрации участников."
           />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+          <MyCard
+            glav="Сайт на .NET для записи к доктору"
+            description="Приложение для записи к врачу, использующее C#, архитектуру MVC и базу данных с авторизацией."
           />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <MyCard
+            glav="Функционирующая база данных на SQL"
+            description="База данных для записи врачей, пациентов и их заболеваний с 3-й нормальной формой."
+          />
+          <MyCard
+            glav="Cайт на WordPress"
+            description="Сайт на платформе WordPress с базой данных на Spaceweb."
+          />
+          <MyCard
+            glav="Создание деканата на Windows Forms"
+            description="Приложение на C# для управления учителями, учениками и их оценками с базой данных на SQL."
+          />
+        </div>
+        <div className={styles.introduction}>
+          <List id="contact"
+          header={<div>Контакты</div>} 
+          bordered
+          
+          />
+        </div>
+      </Layout>
+    </>
   );
-}
+};
+
+export default Home;
